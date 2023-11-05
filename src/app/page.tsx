5379664c749f891959d1605ddea87b6a899a4389
Josh Daniel <joshxfi.dev@gmail.com>
2023-11-05 16:46:04 +0800

    chore: add custom upload button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import frame from "../../public/frame.png";
 export default function Home() {
   const [profilePic, setProfilePic] = useState("");
   const ref = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleProfilePicChange: React.ChangeEventHandler<HTMLInputElement> = (
     e
@@ -53,45 +54,61 @@ export default function Home() {
 
   return (
     <main className="flex items-center flex-col justify-center min-h-screen">
-      <div className="flex space-x-4 mb-12">
-        <input type="file" accept="image/*" onChange={handleProfilePicChange} />
+      <input
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+        onChange={handleProfilePicChange}
+        className="hidden"
+      />
+
+      <div className="overflow-hidden rounded-2xl">
+        <div ref={ref} className="relative overflow-hidden h-[500px] w-[500px]">
+          <Image
+            priority
+            quality={100}
+            src={frame}
+            placeholder="blur"
+            height={500}
+            width={500}
+            className="object-contain absolute pointer-events-none z-10"
+            alt="Profile Picture"
+          />
+
+          {profilePic && (
+            <Draggable>
+              <Resizable>
+                <Image
+                  quality={100}
+                  src={profilePic}
+                  height={500}
+                  width={500}
+                  className="object-contain"
+                  alt="Profile Picture"
+                  draggable={false}
+                />
+              </Resizable>
+            </Draggable>
+          )}
+        </div>
       </div>
 
-      <div ref={ref} className="relative overflow-hidden h-[500px] w-[500px]">
-        <Image
-          priority
-          quality={100}
-          src={frame}
-          placeholder="blur"
-          height={500}
-          width={500}
-          className="object-contain absolute pointer-events-none z-10"
-          alt="Profile Picture"
-        />
+      <div className="mt-12 space-x-3">
+        <button
+          onClick={() => inputRef.current?.click()}
+          className="bg-slate-800 px-5 py-2 rounded"
+        >
+          Upload Photo
+        </button>
 
-        {profilePic && (
-          <Draggable>
-            <Resizable>
-              <Image
-                quality={100}
-                src={profilePic}
-                height={500}
-                width={500}
-                className="object-contain"
-                alt="Profile Picture"
-                draggable={false}
-              />
-            </Resizable>
-          </Draggable>
-        )}
+        <button
+          disabled={!profilePic}
+          onClick={saveImage}
+          className="bg-purple-500 rounded px-5 py-2 disabled:bg-purple-500/50 disabled:cursor-not-allowed"
+        >
+          Save Image
+        </button>
       </div>
-
-      <button
-        onClick={saveImage}
-        className="bg-blue-600 rounded px-5 py-2 mt-12"
-      >
-        Save Image
-      </button>
     </main>
   );
 }
